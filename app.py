a6f904c4eb67277f5b72425ef60186ae83eca535
joa-sabata <joasabata@gmail.com>
2026-05-30 17:31:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # ================= SECCIÓN DE ENCABEZADO LIMPIO =================
-st.title("Homero")
+st.title("Homero 🍩")
 st.write("El procesador automático que hace el trabajo pesado por vos.")
 st.write("---")
 st.write("Pegá tus órdenes acá abajo para separarlas por panel automáticamente.")
@@ -82,7 +82,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown('<div class="col-verde">', unsafe_allow_html=True)
-    btn_procesar = st.button("PROCESAR TODO", use_container_width=True, key="btn_proc")
+    btn_procesar = st.button("✨ PROCESAR TODO", use_container_width=True, key="btn_proc")
     st.markdown('</div>', unsafe_allow_html=True)
 
 with col2:
